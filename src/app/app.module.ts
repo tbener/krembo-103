44aac3d65ed0851dccee9d1e88fb03ca4e73f3ff
Tal Bener <tbener@hotmail.com>
2023-05-13 22:28:26 +0300

    - add @angular/pwa - upgrade to angular 16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -13,6 +13,7 @@ import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { createTranslateLoader } from './services/translations.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,12 @@ import { createTranslateLoader } from './services/translations.service';
         useFactory: (createTranslateLoader),  // <--- add this
         deps: [HttpClient] // <--- add this
       } // <--- add this
-    }) // <--- add this
+    }), ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+}) // <--- add this
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
